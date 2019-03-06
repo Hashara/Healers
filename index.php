b5a2dd68aa6851bb692c0c7942af7f882010ce45
Hashara <47107459+Hashara@users.noreply.github.com>
2019-03-06 18:07:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/_bootswatch.scss">
+    <link rel="stylesheet" href="css/_variables.scss">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Helares</title>
+</head>
+<body>
+    <div class="First">
+        <form action="index.php" method="post">
+        
+          <center> 
+           
+           
+                <legend><h1><b>Healers</b></h1></legend>
+                <p><input type="button" class="btn btn-primary" value="Log In" onclick="window.location.href='login.php'" /></p>
+                <p><input type="button" class="btn btn-primary" value="Sign Up" onclick="window.location.href='login.php'" /></p>
+                <p><input type="button" class="btn btn-primary" value="Visit" onclick="window.location.href='login.php'" /></p>
+                
+            </center>
+        </form>
+    </div> <!--.login-->
+
+</body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,89 @@
+<?php 
+    require_once('inc/connection.php');
+?>
+<?php
+    //submission?
+    if (isset($_POST['submit'])){
+        $errors=array();
+
+        //username and password entered?
+        if(!isset($_POST['studentId']) || strlen(trim($_POST['studentId']))<1){
+            $errors[]="Student ID is missing/Invalid";
+        }
+
+        if(!isset($_POST['password']) || strlen(trim($_POST['password']))<1){
+            $errors[]="password is missing/Invalid";
+        }
+
+        //any errors?
+        if(empty($errors)){
+            //save username and password for variables
+            $studentId=mysqli_real_escape_string($connection,$_POST['studentId']);
+            $password=mysqli_real_escape_string($connection,$_POST['password']);
+            $hashed_password=sha1($password);
+
+            //prepare database query
+            $query= "SELECT * FROM studentdetails WHERE studentId='{$studentId}' AND password='{$hashed_password}' LIMIT 1";
+            
+            $result_set=mysqli_query($connection,$query);
+            //valid?
+            if($result_set){
+                //query sucess
+                
+                if(mysqli_num_rows($result_set)==1){
+                    //vaild user found
+                    header('location:student.php');//rederecting student.php page
+                }else{
+                    //username or password invaild
+                    $errors[]="invaild username or password";
+                }
+
+            }else{
+                $errors[]="Database query failed";
+                //if not desplay error
+            }
+            
+
+
+            
+        }
+    }
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/_bootswatch.scss">
+    <link rel="stylesheet" href="css/_variables.scss">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>LogIn-Helares</title>
+</head>
+<body>
+    <div class="login">
+        <form action="index.php" method="post">
+        
+          <center> 
+           <fieldset> 
+           
+                <legend><h1><b>LogIn</b></h1></legend>
+                
+                <p for="">Addmission number :</p>
+                <input type="text" name="studentId" id="" placeholder="studentId"> 
+                <p><br></p>
+                <p for="">Password :</p>
+                <input type="password" name="password" id="" placeholder="Password">
+                <p><br></p>
+
+                <p><button type="submit" name="submit" class="btn btn-primary">LogIn</button></p>
+            </fieldset>
+            </center>
+        </form>
+    </div> <!--.login-->
+
+</body>
+</html>
+
